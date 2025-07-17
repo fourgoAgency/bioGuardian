d44e9ac9208ad991d81fb7f1bad056d86a4cb375
fourgoAgency <fourgo.agency@gmail.com>
2025-07-17 07:02:00 +0500

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
         <LanguageProvider>
           <CartProvider>
             <QueryProvider>
+              
               {children}
             </QueryProvider>
           </CartProvider>
