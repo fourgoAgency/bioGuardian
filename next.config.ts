0a8015dd21671294b8f727956748ac4b00c791c1
fourgoAgency <fourgo.agency@gmail.com>
2025-07-25 13:11:35 +0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/**',
       }
     ],
   },
