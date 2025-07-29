309f9c326fce35a15d7f91cbe091154bfae3dde8
fourgoAgency <fourgo.agency@gmail.com>
2025-07-29 22:09:57 +0500

    Add files via upload

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  storageBucket: 'bioguardian-website.firebasestorage.app',
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
@@ -18,7 +18,7 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 const auth = getAuth(app);
-const storage = getStorage(app) // ✅ Now added
+const storage = getStorage(app)
 
 
-export { db, auth, storage}; // ✅ Now exporting storage
+export { db, auth, storage}; 
