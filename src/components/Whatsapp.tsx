3f6ba0d5a23b8bfa2262f54cd423185c0b65045d
fourgoAgency <fourgo.agency@gmail.com>
2025-07-29 22:19:27 +0500

    Update Whatsapp.tsx

--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import Whatsapp_image from '@/../public/WhatsApp.png'
+import Whatsapp_image from '@/../public/Whatsapp.png'
 
 interface WhatsappProps {
   whatsappLink?: string;
