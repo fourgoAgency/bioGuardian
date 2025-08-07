7c0950a92aa44e9fbeab445b707a7fddd80625e4
fourgoAgency <fourgo.agency@gmail.com>
2025-08-07 20:10:19 +0500

    Update OverviewCard.tsx

--- src/components/admin/OverviewCard.tsx
+++ src/components/admin/OverviewCard.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
 
-export default function OverviewCard({ title, Icon, value, timeframe, link }: { title: string; Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>; value: string | number; timeframe: string, link: string }) {
+export default function OverviewCard({ title, Icon, value, timeframe }: { title: string; Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>; value: string | number; timeframe: string}) {
   return (
     <div className="bg-white shadow-xl border rounded-xl p-4 m-4">
-      <Link href={link}>
       <h3 className="text-sm text-gray-500">{title}</h3>
       <div className="flex items-baseline justify-between mt-2">
         <p className="text-2xl font-bold mt-1 text-blue-600">{value}</p>
@@ -13,8 +11,8 @@ export default function OverviewCard({ title, Icon, value, timeframe, link }: { 
         </span>
       </div>
       <span className="text-xs text-gray-400">{timeframe}</span>
-      </Link>
     </div>
   );
 }
+
 
